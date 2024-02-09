c8bb8e6177c7f491bbc1dfec9f6d5875457e3fb0
krtt <tokertu@gmail.com>
2024-02-09 18:42:09 +0200

    P1 final

--- practice_1/nodes/publisher.py
+++ practice_1/nodes/publisher.py
@@ -4,9 +4,16 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node('publisher')
-rate = rospy.Rate(2)
+
 pub = rospy.Publisher('/message', String, queue_size=10)
 
+message = rospy.get_param('~message', 'Hello World Default!')
+hz      = rospy.get_param('~hz', 2)
+ 
+rate = rospy.Rate(hz)
+
+
 while not rospy.is_shutdown():
-    pub.publish("Hello world!")
+    pub.publish(message)
     rate.sleep()
+    
